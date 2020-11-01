c38c14ff9dda3621f7bec5112d361a6155f753ec
matheusfalves <matheusf.alves1@hotmail.com>
2020-11-01 19:46:06 -0200

    Class main, que rodara tudo, no momento so tem um scanner

--- APS_JAPA/src/game/Main.java
+++ APS_JAPA/src/game/Main.java
@@ -1,16 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package game;
 
-/**
- *
- * @author Matheus
- */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Digite o nome do player");
+        Player P1 = new Player(entrada.nextLine());
+        
         
     }
 }
